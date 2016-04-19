f19318503b78f26f261767863b1209c37ef18baf
Laurent <lg390@cam.ac.uk>
2016-04-20 00:28:12 +0100

    search GO unit test

---
+++ tests/testthat/test_searchGO.R
@@ -0,0 +1,13 @@
+test_that("searchGO function", {
+    i <- FALSE
+    xcc <- x <- searchGO("Golgi apparatus", "CC", interactive = i)
+    expect_length(x, 3)
+    x <- searchGO("^Golgi apparatus$", "CC", interactive = i) ## exact matching
+    expect_identical(x, "GO:0005794")
+    xmf <- searchGO("Golgi apparatus", "MF", interactive = i)
+    expect_length(xbp, 8)
+    xbp <- searchGO("Golgi apparatus", "BP", interactive = i)
+    expect_null(xmf)
+    x <- searchGO("Golgi apparatus", interactive = i)
+    expect_identical(sort(x), sort(c(xcc, xmf, xbp)))
+})
