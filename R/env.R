cdb2ef8db2ace19ee86d1ee829a6881875103e63
Laurent <lg390@cam.ac.uk>
2016-04-20 00:08:06 +0100

    new env with private accessor functions

--- R/env.R
+++ R/env.R
@@ -1,8 +1,25 @@
 yagopEnv <- new.env(parent = emptyenv())
 
-sql <- "SELECT go_id FROM go_term WHERE ontology IN"
-for (ns in c("BP", "MF", "CC")) {
-    ids <- dbGetQuery(GO_dbconn(),
-                      paste(sql, "('", ns, "');", sep = ""))$go_id
-    assign(paste0("GO", ns, "TERM"), GOTERM[ids], yagopEnv)
+
+prepareGOTermsInEnv <- function() {
+    sql <- "SELECT go_id FROM go_term WHERE ontology IN"
+    for (ns in c("BP", "MF", "CC")) {
+        ids <- dbGetQuery(GO_dbconn(),
+                          paste(sql, "('", ns, "');", sep = ""))$go_id
+        assign(paste0("GO", ns, "TERM"),
+               GOTERM[ids],
+               yagopEnv)
+    }
 }
+
+GOCCTerms <- function() yagopEnv$GOCCTERM
+GOCCList <- function() as.list(GOCCTerms())
+
+GOMFTerms <- function() yagopEnv$GOMFTERM
+GOMFList <- function() as.list(GOMFTerms())
+
+GOBPTerms <- function() yagopEnv$GOBPTERM
+GOBPList <- function() as.list(GOBPTerms())
+
+GOTerms <- function() GO.db::GOTERM
+GOList <- function() as.list(GOTerms())
